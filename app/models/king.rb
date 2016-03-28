f48f1c062c9402d44dfcb0f3f7967a7a9bb56753
Patrick Shushereba <pshushereba@gmail.com>
2016-03-28 04:00:38 +0000

    Started planning valid_move? method.

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,2 +1,13 @@
 class King < Piece
+
+	# Should determine if the king piece can move to the designated spot
+	def valid_move?(destination_row, destination_col)
+		# Call obstructed method to see if there is already a piece in the
+		# destination_row/destination_column
+		return false if obstructed?(destination_row, destination_col)
+
+		# Write code to make the piece move.
+
+	end
+
 end
--- spec/models/king_spec.rb
+++ spec/models/king_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 RSpec.describe King, type: :model do
 	describe 'valid_move?' do
-
+		it 'should allow a vertical-up move' do
+			game = FactoryGirl.create(:game)
+		end
 	end
 
 	end
