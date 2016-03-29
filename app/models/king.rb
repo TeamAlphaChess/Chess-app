efcb3bf1374e16a06488f89a633a4d6effa3a397
megankbarker <megankbarker@gmail.com>
2016-03-29 22:26:19 +0000

    Continue with method logic

--- app/models/king.rb
+++ app/models/king.rb
@@ -4,10 +4,19 @@ class King < Piece
 	def valid_move?(destination_row, destination_col)
 		# Call obstructed method to see if there is already a piece in the
 		# destination_row/destination_column
-		return false if obstructed?(destination_row, destination_col)
+    #if abs(current_row_index, destination_row, current_column_index, destination_col)
+      return false unless abs(destination_row - current_row_index) <= 1 
+      return false unless abs(destination_col - current_column_index) <= 1 
+		  return false if obstructed?(destination_row, destination_col)
 
 		# Write code to make the piece move.
-
+    # Reset king current_column_index and current_row_index to destination_col and destination_row
+    if true
+      current_row_index = destination_row
+      current_column_index = destination_col
+    end
 	end
+
+  # Write abs method to evaluate distance from king
 
 end
--- spec/models/king_spec.rb
+++ spec/models/king_spec.rb
@@ -3,7 +3,12 @@ RSpec.describe King, type: :model do
 	describe 'valid_move?' do
 		it 'should allow a vertical-up move' do
 			game = FactoryGirl.create(:game)
+      
 		end
+
+
+    it 'should not move an obstructed space' do
+      expect(game.obstructed?).to eq false
 	end
 
 	end
