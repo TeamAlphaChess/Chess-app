31ea6de3b0fcf9a5736360a0c98d494cde6071e9
Brett Wood <brett_wood@outlook.com>
2016-04-03 09:29:58 +0000

    footer, sidenav,media queries added to forms. Still need to implement ajax and errors

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -2,6 +2,11 @@ $(document).ready(function() {
 
     $('#signUpModalTrigger').on('click', function() {
       $('#signUpModal').modal('toggle');
-      $('.modal').css({'display': 'flex', 'align-items': 'center', 'justify-content': 'center'});
+      $('#signUpModal').addClass('modal-flexbox');
+    });
+
+    $('#loginModalTrigger').on('click', function() {
+      $('#loginModal').modal('toggle');
+      $('#loginModal').addClass('modal-flexbox');
     });
 });
