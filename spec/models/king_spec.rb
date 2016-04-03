7268635a0e89c4c28688840ffd51e84098be9baa
megankbarker <megankbarker@gmail.com>
2016-04-03 21:14:03 +0000

    Fix rubocop indentation offenses

--- spec/models/king_spec.rb
+++ spec/models/king_spec.rb
@@ -2,51 +2,51 @@ require 'rails_helper'
 RSpec.describe King, type: :model do
 	describe 'valid_move?' do
     it 'should allow a vertical-up move' do
-  	  game = FactoryGirl.create(:game)
-  		king = King.new(game: game, current_row_index: 5, current_column_index: 5)
-  		expect(king.valid_move?(6, 5)).to eq true
+      game = FactoryGirl.create(:game)
+      king = King.new(game: game, current_row_index: 5, current_column_index: 5)
+      expect(king.valid_move?(6, 5)).to eq true
   	end
 
     it 'should allow a vertical-down move' do
-  		game = FactoryGirl.create(:game)
-  		king = King.new(game: game, current_row_index: 5, current_column_index: 5)
-  		expect(king.valid_move?(6, 5)).to eq true
+      game = FactoryGirl.create(:game)
+      king = King.new(game: game, current_row_index: 5, current_column_index: 5)
+      expect(king.valid_move?(6, 5)).to eq true
   	end
 
   	it 'should allow a horizonal-left move' do
-  		game = FactoryGirl.create(:game)
-  		king = King.new(game: game, current_row_index: 5, current_column_index: 5)
-  		expect(king.valid_move?(5, 4)).to eq true
-  	end
+      game = FactoryGirl.create(:game)
+      king = King.new(game: game, current_row_index: 5, current_column_index: 5)
+      expect(king.valid_move?(5, 4)).to eq true
+    end
 
   	it 'should allow a horizonal-right move' do
-  		game = FactoryGirl.create(:game)
-  		king = King.new(game: game, current_row_index: 5, current_column_index: 5)
-  		expect(king.valid_move?(5, 6)).to eq true
-  	end
+      game = FactoryGirl.create(:game)
+      king = King.new(game: game, current_row_index: 5, current_column_index: 5)
+      expect(king.valid_move?(5, 6)).to eq true
+    end
 
     it 'should allow a diagonal-up-right move' do
-  		game = FactoryGirl.create(:game)
-  		king = King.new(game: game, current_row_index: 5, current_column_index: 5)
-  		expect(king.valid_move?(4, 6)).to eq true
+      game = FactoryGirl.create(:game)
+      king = King.new(game: game, current_row_index: 5, current_column_index: 5)
+      expect(king.valid_move?(4, 6)).to eq true
   	end
 
     it 'should allow a diagonal-up-left move' do
-  		game = FactoryGirl.create(:game)
-  		king = King.new(game: game, current_row_index: 5, current_column_index: 5)
-  		expect(king.valid_move?(5, 6)).to eq true
+      game = FactoryGirl.create(:game)
+      king = King.new(game: game, current_row_index: 5, current_column_index: 5)
+      expect(king.valid_move?(5, 6)).to eq true
   	end
 
     it 'should allow a diagonal-down-right move' do
-  		game = FactoryGirl.create(:game)
-  		king = King.new(game: game, current_row_index: 5, current_column_index: 5)
-  		expect(king.valid_move?(6, 6)).to eq true
+      game = FactoryGirl.create(:game)
+      king = King.new(game: game, current_row_index: 5, current_column_index: 5)
+      expect(king.valid_move?(6, 6)).to eq true
   	end
 
     it 'should allow a diagonal-down-left move' do
-  		game = FactoryGirl.create(:game)
-  		king = King.new(game: game, current_row_index: 5, current_column_index: 5)
-  		expect(king.valid_move?(6, 4)).to eq true
+      game = FactoryGirl.create(:game)
+      king = King.new(game: game, current_row_index: 5, current_column_index: 5)
+      expect(king.valid_move?(6, 4)).to eq true
   	end
 
     it 'should allow a move to a space if piece is opposite color' do
@@ -62,6 +62,5 @@ RSpec.describe King, type: :model do
       black_pawn = FactoryGirl.create(:pawn, game: game, current_row_index: 4, current_column_index: 5, color: 'black')
       expect(black_king.valid_move?(4,5)).to eq false
     end
-
   end
 end
