d717e864d07451192fc5dc1bb4408f4b99659ecf
megankbarker <megankbarker@gmail.com>
2016-04-03 22:24:19 +0000

    Fix identation errors

--- spec/models/king_spec.rb
+++ spec/models/king_spec.rb
@@ -5,21 +5,21 @@ RSpec.describe King, type: :model do
       game = FactoryGirl.create(:game)
       king = King.new(game: game, current_row_index: 5, current_column_index: 5)
       expect(king.valid_move?(6, 5)).to eq true
-  	end
+    end
 
     it 'should allow a vertical-down move' do
       game = FactoryGirl.create(:game)
       king = King.new(game: game, current_row_index: 5, current_column_index: 5)
       expect(king.valid_move?(6, 5)).to eq true
-  	end
+    end
 
-  	it 'should allow a horizonal-left move' do
+    it 'should allow a horizonal-left move' do
       game = FactoryGirl.create(:game)
       king = King.new(game: game, current_row_index: 5, current_column_index: 5)
       expect(king.valid_move?(5, 4)).to eq true
     end
 
-  	it 'should allow a horizonal-right move' do
+    it 'should allow a horizonal-right move' do
       game = FactoryGirl.create(:game)
       king = King.new(game: game, current_row_index: 5, current_column_index: 5)
       expect(king.valid_move?(5, 6)).to eq true
@@ -29,25 +29,25 @@ RSpec.describe King, type: :model do
       game = FactoryGirl.create(:game)
       king = King.new(game: game, current_row_index: 5, current_column_index: 5)
       expect(king.valid_move?(4, 6)).to eq true
-  	end
+    end
 
     it 'should allow a diagonal-up-left move' do
       game = FactoryGirl.create(:game)
       king = King.new(game: game, current_row_index: 5, current_column_index: 5)
       expect(king.valid_move?(5, 6)).to eq true
-  	end
+    end
 
     it 'should allow a diagonal-down-right move' do
       game = FactoryGirl.create(:game)
       king = King.new(game: game, current_row_index: 5, current_column_index: 5)
       expect(king.valid_move?(6, 6)).to eq true
-  	end
+    end
 
     it 'should allow a diagonal-down-left move' do
       game = FactoryGirl.create(:game)
       king = King.new(game: game, current_row_index: 5, current_column_index: 5)
       expect(king.valid_move?(6, 4)).to eq true
-  	end
+    end
 
     it 'should allow a move to a space if piece is opposite color' do
       game = FactoryGirl.create(:game)
@@ -62,5 +62,6 @@ RSpec.describe King, type: :model do
       black_pawn = FactoryGirl.create(:pawn, game: game, current_row_index: 4, current_column_index: 5, color: 'black')
       expect(black_king.valid_move?(4,5)).to eq false
     end
+
   end
 end
