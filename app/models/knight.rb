c11ba0979c93aef7b55349a5ad579d403757a02f
Harsheek Thanki <harsheekthanki@gmail.com>
2016-04-08 21:17:16 +0000

    Add code to valid_move? method in knight.rb (Work in progress)

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,6 +1,20 @@
 class Knight < Piece
 
-  def valid_move?
+  def valid_move?(destination_row, destination_col)
+    if same_color?(destination_row, destination_col)
+      # Move fails
+      false
+    elsif valid_coordinates(current_column_index, current_row_index, destination_col, destination_row)
+      # Move passes
+      true
+    else
+      false
+    end
+
+    # def valid_coordinates(x1, y1, x2, y2)
+    #   true if (x1-x2).abs == 1 && (y1 - y2).abs == 2
+    #   true if (x1-x2).abs == 2 && (y1 - y2).abs == 1
+    #   false
+    # end
   end
-  
 end
