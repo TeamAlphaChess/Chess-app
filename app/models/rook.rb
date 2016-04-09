ee4deba8c23fae589419bd45e424d8a574e19bbe
Harsheek Thanki <harsheekthanki@gmail.com>
2016-04-09 20:47:11 +0000

    Method call ordering adjustment in rook.rb. 2 more tests pass

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,10 +1,11 @@
 class Rook < Piece
   def valid_move?(destination_row, destination_col)
     # enter code here
+    return true if horizontal?(destination_row, destination_col)
+    return true if vertical?(destination_row, destination_col)  
     return false if obstructed?(destination_row, destination_col) 
     return false if same_color?(destination_row, destination_col)
-    return true if horizontal?(destination_row, destination_col)
-    return true if vertical?(destination_row, destination_col)
+    
     if !spot_taken?(destination_row, destination_col) || !same_color?(destination_row, destination_col)
       true
     end
