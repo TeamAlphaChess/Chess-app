21b0d6e53d1d6489d55a5536d983a815ec4553ba
megankbarker <megankbarker@gmail.com>
2016-04-09 12:02:29 +0000

    Build valid_move? and rook_spec

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,5 +1,12 @@
 class Rook < Piece
-  def valid_move?
+  def valid_move?(destination_row, destination_col)
     # enter code here
+    return false if obstructed?(destination_row, destination_col) 
+    return false if same_color?(destination_row, destination_col)
+    return true if horizontal?(destination_row, destination_col)
+    return true if vertical?(destination_row, destination_col)
+    if !spot_taken?(destination_row, destination_col) || !same_color?(destination_row, destination_col)
+      true
+    end
   end
 end
