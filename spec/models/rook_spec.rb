8472b33ffc8ef8ec9177b0808dd8a904f310fa25
Harsheek Thanki <harsheekthanki@gmail.com>
2016-04-09 22:26:45 +0000

    Complete tests for rook valid_move? method

--- spec/models/rook_spec.rb
+++ spec/models/rook_spec.rb
@@ -42,39 +42,19 @@ RSpec.describe Rook, type: :model do
 
     it 'should return true for a move to a space with different color piece' do
       game = FactoryGirl.create(:game)
-      # white_rook = game.pieces.find_by_current_row_index_and_current_column_index(0, 0)
-      # white_pawn = game.pieces.find_by_current_row_index_and_current_column_index(1, 0)
-      # expect(white_rook.valid_move?(1, 0)).to eq false
-    end
-
-
-
-
-    it 'should return false for a move to a space if path is obstructed' do
-      game = FactoryGirl.create(:game)
-      # Select white rook from the board
       white_rook = game.pieces.find_by_current_row_index_and_current_column_index(0, 0)
-      # Select black pawn from the board 
-      black_pawn = game.pieces.find_by_current_row_index_and_current_column_index(6, 0)
-      # find/select white pawn that obstructs??
-
-      # Expect white king's move to black pawn's space to be invalid because white pawn obstructs
-      expect(white_rook.valid_move?(6, 0)).to eq false
-
-    end
-
-    it 'should NOT allow an unobstructed move if piece is same color' do
-      game = FactoryGirl.create(:game)
-      # Select white rook from the board
-      white_rook = game.pieces.find_by_current_row_index_and_current_column_index(0, 0)
-      # Select white pawn from the board
       white_pawn = game.pieces.find_by_current_row_index_and_current_column_index(1, 0)
-      # Update white pawn's position
-      white_pawn.update_attributes(current_row_index: 5, current_column_index: 0)
-      # Expect white king's move to white pawn's space to be invalid
-      expect(white_rook.valid_move?(5, 0)).to eq false
-
+      white_pawn.update_attributes(current_row_index: nil, current_column_index: nil)
+      black_pawn = game.pieces.find_by_current_row_index_and_current_column_index(6, 0)
+      black_pawn.update_attributes(current_row_index: 1, current_column_index: 0)
+      expect(white_rook.valid_move?(1, 0)).to eq true
     end
 
+    it 'should return false for an obstructed move to an empty spot' do
+      game = FactoryGirl.create(:game)
+      white_rook = game.pieces.find_by_current_row_index_and_current_column_index(0, 0)
+      white_pawn = game.pieces.find_by_current_row_index_and_current_column_index(1, 0)
+      expect(white_rook.valid_move?(2, 0)).to eq false
+    end
   end
 end
