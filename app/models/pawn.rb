92b41e34eaa6e274ac35b40eab1ffd9c41f42565
Harsheek Thanki <harsheekthanki@gmail.com>
2016-04-11 21:09:11 +0000

    Add en_passant pseudo-code to pawn.rb

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -35,4 +35,20 @@ class Pawn < Piece
       true
     end
   end
+
+  def en_passant
+  # en passant pseudo-code:
+
+  # store last_move in database
+  # if last_move = opponent pawn moved forward by 2 squares
+
+  # and column of opponent pawn is now = current pawn column + or - 1 (i.e. is adjacent)
+
+  # and row of opponent = current pawn row
+
+  # then current pawn moves to opponent pawn column, and - 1 row if current pawn is black or +1 row if current pawn is white
+
+  # and opponent pawn gets captured (current_row_index: nil, current_column_index: nil, captured: true)
+  end
+
 end
