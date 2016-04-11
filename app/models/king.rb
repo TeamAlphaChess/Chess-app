b81fb3010e885e50a69527d350d4a7f9f583b591
Patrick Shushereba <pshushereba@gmail.com>
2016-04-11 01:41:17 +0000

    Defined castle methods in king.rb

--- app/models/king.rb
+++ app/models/king.rb
@@ -7,4 +7,14 @@ class King < Piece
     # Check to see if the king is only moving exactly one space in any direction on the board
     !same_color?(destination_row, destination_col) && distance(destination_row, destination_col) == 1
   end
+
+  def can_castle?(rook_position)
+    # this method will check to make sure that the rook and the
+    # king have not moved.
+  end
+
+  def castle!(rook_position)
+    # this is where we will update the database for the move.
+  end
+
 end
