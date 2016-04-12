7a06a0095458fe62be2fde0509834e51e1477ac2
megankbarker <megankbarker@gmail.com>
2016-04-12 07:10:56 +0000

    Add frozen_string_literal true to users_helper.rb and user.rb

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+module UsersHelper
+  # Returns the Gravatar for the given user
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    gravatar_url = "http://www.gravatar.com/avatar/#{gravatar_id}?d=mm"
+    image_tag(gravatar_url, alt: user.name, class: 'navbar-avatar')
+  end
+end
