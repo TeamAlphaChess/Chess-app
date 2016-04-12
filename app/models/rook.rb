b31477dc66f100160e54a3437b072a42feac8340
Harsheek Thanki <harsheekthanki@gmail.com>
2016-04-12 16:57:26 +0000

    Add a diagonal move check return false in rook.rb

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,6 +1,7 @@
 class Rook < Piece
   def valid_move?(destination_row, destination_col)
     # enter code here
+    return false if diagonal?(destination_row, destination_col)
     return false if obstructed?(destination_row, destination_col)
     return false if same_color?(destination_row, destination_col)
 
