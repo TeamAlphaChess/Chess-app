c8419cd9c92996a3e64ce3f6e12aee2caa44f3df
Harsheek Thanki <harsheekthanki@gmail.com>
2016-04-12 19:25:06 +0000

    Disable rubocop for Cyclomatic Complexity for lines 4-7 in rook.rb

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 class Rook < Piece
+  # rubocop:disable Metrics/CyclomaticComplexity
   def valid_move?(destination_row, destination_col)
-    # enter code here
     return false if diagonal?(destination_row, destination_col)
     return false if obstructed?(destination_row, destination_col)
     return false if same_color?(destination_row, destination_col)
+    # rubocop:enable Metrics/CyclomaticComplexity
 
     return true if empty_or_diff_color?(destination_row, destination_col) && (horizontal?(destination_row, destination_col) || vertical?(destination_row, destination_col))
   end
