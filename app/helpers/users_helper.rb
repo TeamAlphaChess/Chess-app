c6b46437065dfb16e405d3cc259fc4ee32e40611
megankbarker <megankbarker@gmail.com>
2016-04-18 03:27:44 +0000

    Fix rubocop errors

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
+# frozen_string_literal: true
 module UsersHelper
   # Returns the Gravatar for the given user
   def gravatar_for(user)
-    default_url = "#{root_url}/icons/avatar.svg"
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}.png?d=mm"
     image_tag(gravatar_url, alt: user.name, class: 'navbar-avatar')
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 FactoryGirl.define do
   factory :avatar do
-    
   end
   factory :user do
     sequence :email do |n|
