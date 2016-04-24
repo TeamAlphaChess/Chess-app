4994200b4788a22ebd091b72561e0e1a64396674
Harsheek Thanki <harsheekthanki@gmail.com>
2016-04-24 17:33:37 +0000

    Write one more test for games#forfeit action in games_controller_spec.rb

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -38,6 +38,19 @@ RSpec.describe GamesController, type: :controller do
     end
 
     it 'should increment the other players games_won count by 1 when forfeiting' do
+      user = FactoryGirl.create(:user)
+      second_user = FactoryGirl.create(:user)
+      sign_in user
+      sign_in second_user
+      game = FactoryGirl.create(:game)
+      user.id == game.white_player_id
+      second_user.id == game.black_player_id
+      second_user.update_attributes(games_won: 0)
+      game.current_player_turn_id == game.white_player_id
+
+      get :forfeit, id: game.id
+      user.forfeit
+      expect(second_user.games_won).to eq 1
     end
 
     it 'should return false if a user tries to forfeit a game they are not a part of' do
